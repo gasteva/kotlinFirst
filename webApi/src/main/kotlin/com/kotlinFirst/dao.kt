b5142e1eaef2b82d7cce938f0479bf0a55c3235b
Galina Gastev <gasteva@gmail.com>
2017-09-04 00:34:25 +0300

    update structure

---
+++ webApi/src/main/kotlin/com/kotlinFirst/dao.kt
@@ -0,0 +1,13 @@
+package com.kotlinFirst.dao
+
+import java.util.*
+
+data class User( val id: Int,
+                 val NickName: String?)
+
+data class Twitt(val id: Int,
+                 val createDate: Date,
+                 val text: String?)
+
+data class following(val originId: Int, val followee: List<Int>?)
+
